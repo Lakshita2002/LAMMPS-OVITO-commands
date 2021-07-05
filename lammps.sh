37ff87f34a51edae17ab527b97c3488c0b51f10f
Lakshita2002 <lakshitamohanty2002@gmail.com>
2021-07-05 20:35:16 +0530

    added bash scripts

---
+++ lammps.sh
@@ -0,0 +1,8 @@
+# to run a lammps script, go to the root lammps directory
+# for reference - box particles folder 
+
+# this is where the script file is located
+cd ~/mylammps/examples/box_2particles/box_2particles
+
+# replace script.in with the input script you want to run
+../../../src/lmp_serial -i script.in
---
+++ ovito.sh
@@ -0,0 +1,5 @@
+# go to the root directory of ovito
+cd ~/Downloads/ovito-basic-3.5.2-x86_64/
+
+# run the executable in the bin folder
+./bin/ovito
